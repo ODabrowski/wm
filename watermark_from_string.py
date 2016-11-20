72489f793e188830ab8063f0487b8108e04634f1
ODabrowski <od_1986@hotmail.com>
2016-11-20 18:42:29 +0100

    Watermark image from String
    Watermark image from String (convert string to image)

---
+++ watermark_from_string.py
@@ -0,0 +1,20 @@
+from PIL import Image
+from PIL import ImageDraw
+from PIL import ImageFont
+
+
+fileName=input("Name of file to be watermarked...")
+posX=int(input("X position of text..."))
+posY=int(input("Y position of text..."))
+img = Image.open(fileName)
+d = ImageDraw.Draw(img)
+#possibilite de changer la police de caractere - a etudier
+# font = ImageFont.truetype(<font-file>, <font-size>)
+#font = ImageFont.truetype("sans-serif.ttf", 16)
+# d.text((x, y),"Sample Text",(r,g,b))
+
+##fonts_path = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'fonts')
+##font = ImageFont.truetype(os.path.join(fonts_path, 'sans_serif.ttf'), 24)
+
+d.text((posX, posY),"Sample Text",(255,255,255))
+img.save('result.jpg')
